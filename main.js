3b3df6f9cb871b3f39b2691b4a02b246ec99b8d7
unknown <soniomelga@gmail.com>
2024-06-18 02:24:01 +0800

    Add basic functions for number and operator keys

--- main.js
+++ main.js
@@ -1,64 +1,111 @@
-const calculator = document.querySelector('.calculator')
-const keys = document.querySelector('.calculator_keys')
-const display = document.querySelector('.calculator_display')
+const calculator = document.querySelector('.calculator');
+const keys = document.querySelector('.calculator_keys');
+const display = document.querySelector('.calculator_display');
+
+let keyPressCount = 0;
+let keyPressLog = ['0'];
+
+let operatorPressCount = 0;
+let operatorPressLog = ['+'];
+
+let runningTotalLog = ['0'];
+
+let numKeys = []; // Initialize an empty list of number keys
+for (let i = 0; i <= 9; i++) {
+    numKeys.push(i); // Iterate over a loop that goes through numbers 1 to 9 and push each iterated number to numKeys list
+}
 
 keys.addEventListener('click', e => {
 
     if (e.target.matches('button')) {
+        const key = e.target; // Key being pressed
+        const keyContent = key.textContent; // Content of the key
+        const displayContent = display.textContent; // Content of the display
 
-        const key = e.target
-        const action = key.dataset.action
-        const keyContent = key.textContent
-        const displayedNum = display.textContent
 
-        // If user clicks a number //
-        if (!action) {
-            
-            // If the displayed number is 0, the number pressed will replace it. Else, it will append the previous number.//
-            if (displayedNum === '0' || previousKeyType === 'operator') {
-                display.textContent = keyContent
-            } else {
-                display.textContent = displayedNum + keyContent
+
+        let operatorKeys = ['+', '-', '÷', '*']; // Define operatorKeys variable, which is a list of all operator symbols
+
+        let prevKey = keyPressLog[keyPressCount]; // Define prevKey variable, which is the last element in keyPressLog list
+        let prevOperator = operatorPressLog[operatorPressCount]; // Define prevOperator variable, which is the last element in operatorPressLog list
+        let prevRunningTotal = parseInt(runningTotalLog[operatorPressCount]);
+
+        /*
+        // Experiment Only. Commented to not interfere with functionality.
+
+        display.textContent = keyContent // Display key being pressed
+        runningTotal = parseInt(keyContent) // Assign numerical value of keyContent to result variable
+
+        console.log(runningTotal)
+
+        */
+        
+        // If key is a number 
+        if (numKeys.some(element => element === parseInt(keyContent))) {
+            // If prevKeyType is 'number'
+            if (numKeys.some(element => element === parseInt(prevKey))) {
+                // If displayContent is '0'; key will replace current display content
+                if (displayContent === '0') {
+                    display.textContent = keyContent;
+                }
+                // Else; current key will append display content
+                else {
+                    display.textContent += keyContent;
+                }
+            }
+
+            // If prevKeyType is 'operator', replace the display content with keyContent
+            if (operatorKeys.some(element => element === prevKey)) {
+                display.textContent = keyContent;
             }
             
         }
 
-        // If an operator is pressed, a class of name is-depressed will be added. //
-        if (
-            action === 'add' ||
-            action === 'subtract' ||
-            action === 'multiply' ||
-            action === 'divide'
-          ) {
+        // If key is an operator
+        if (operatorKeys.some(element => element === keyContent)) {  
+            let curRunningTotal = operation(prevRunningTotal, parseInt(displayContent), prevOperator);
+            runningTotalLog.push(curRunningTotal);
 
-            key.classList.add('is-depressed')
-            keys.addEventListener('click', e => {
-                if (e.target.matches('button')) {
-                  const key = e.target
-                  
-                  // Remove .is-depressed class from all keys
-                  Array.from(key.parentNode.children)
-                    .forEach(k => k.classList.remove('is-depressed'))
-                }
-            })
-
-            // Add custom attribute
-            calculator.dataset.previousKeyType = 'operator'
+            display.textContent = curRunningTotal;
+            operatorPressCount += 1;
+            operatorPressLog.push(keyContent);
         }
 
-        // If decimal point is pressed //
-        if (action === 'decimal') {
-            
-            display.textContent = displayedNum + '.'
-        }
-          
-        if (action === 'clear') {
-            //
-        }
-          
-        if (action === 'calculate') {
-            //
-        }
 
+        // If key is a decimal point
+
+
+
+
+
+        // If key is an equal sign
+
+
+
+
+        // If key is AC
+
+
+
+        keyPressCount += 1;
+        keyPressLog.push(keyContent);
     }  
 })
+
+// Define operation function with three params (int, int, str)
+function operation(firstNum, secondNum, operator) {
+
+    if (operator === '+') {
+        return firstNum + secondNum;
+    }
+    if (operator === '-') {
+        return firstNum - secondNum;
+    }
+    if (operator === '*') {
+        return firstNum * secondNum;
+    }
+    if (operator === '÷') {
+        return firstNum / secondNum;
+    }
+}
+
