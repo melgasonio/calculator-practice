64d80f0d3893cdaafd8694e9d3f68368491f82c0
unknown <soniomelga@gmail.com>
2024-06-18 12:30:23 +0800

    Add decimal point feature

--- main.js
+++ main.js
@@ -24,26 +24,17 @@ keys.addEventListener('click', e => {
 
 
 
-        let operatorKeys = ['+', '-', '÷', '*']; // Define operatorKeys variable, which is a list of all operator symbols
+        const operatorKeys = ['+', '-', '÷', '*']; // Define operatorKeys variable, which is a list of all operator symbols
 
         let prevKey = keyPressLog[keyPressCount]; // Define prevKey variable, which is the last element in keyPressLog list
         let prevOperator = operatorPressLog[operatorPressCount]; // Define prevOperator variable, which is the last element in operatorPressLog list
-        let prevRunningTotal = parseInt(runningTotalLog[operatorPressCount]);
+        let prevRunningTotal = parseFloat(runningTotalLog[operatorPressCount]);
 
-        /*
-        // Experiment Only. Commented to not interfere with functionality.
-
-        display.textContent = keyContent // Display key being pressed
-        runningTotal = parseInt(keyContent) // Assign numerical value of keyContent to result variable
-
-        console.log(runningTotal)
-
-        */
         
         // If key is a number 
-        if (numKeys.some(element => element === parseInt(keyContent))) {
+        if (numKeys.includes(parseFloat(keyContent))) {
             // If prevKeyType is 'number'
-            if (numKeys.some(element => element === parseInt(prevKey))) {
+            if (numKeys.includes(parseFloat(prevKey))) {
                 // If displayContent is '0'; key will replace current display content
                 if (displayContent === '0') {
                     display.textContent = keyContent;
@@ -54,25 +45,65 @@ keys.addEventListener('click', e => {
                 }
             }
 
-            // If prevKeyType is 'operator', replace the display content with keyContent
-            if (operatorKeys.some(element => element === prevKey)) {
+            // If previous key is an operator, replace the display content with keyContent
+            if (operatorKeys.includes(prevKey)) {
                 display.textContent = keyContent;
             }
             
+            // If previous key is a decimal point, append the number to the current display.
+            if (prevKey === '.') {
+                display.textContent += keyContent;
+            }
         }
 
-        // If key is an operator
-        if (operatorKeys.some(element => element === keyContent)) {  
-            let curRunningTotal = operation(prevRunningTotal, parseInt(displayContent), prevOperator);
-            runningTotalLog.push(curRunningTotal);
+        // If key is an operator key
+        if (operatorKeys.includes(keyContent)) {
 
-            display.textContent = curRunningTotal;
-            operatorPressCount += 1;
+            // If operator key precedes another operator key
+            if (operatorKeys.includes(prevKey)) {
+                // Remove the last element of operatorPressLog for the current key to replace. 
+                // operation() will not be called and displayed number will not be changed
+                operatorPressLog.splice(operatorPressCount);
+            } else { // Else
+                // Initialize running total's value as the return value of operation() with given parameters.
+                let curRunningTotal = operation(prevRunningTotal, parseFloat(displayContent), prevOperator);
+
+                // Since there is a change in running total, this will be added/pushed to runningTotalLog list.
+                runningTotalLog.push(curRunningTotal);
+
+                // Display current running total
+                display.textContent = curRunningTotal;
+
+                // Every operator key press that does not follow another operator key is considered to be unique. operatorPressCount needs to increase by 1 for every instance of this.
+                operatorPressCount += 1;
+            }
+
+            // Push keyContent (operator key) to operatorPressLog
             operatorPressLog.push(keyContent);
         }
 
 
         // If key is a decimal point
+        if (keyContent === '.') {
+            // If a decimal point does not follow another decimal point
+            if (prevKey !== '.') {
+                // If decimal key follows a number key
+                if (numKeys.includes(parseFloat(prevKey))) {
+                    // If displayed number has no decimal point
+                    if (!displayContent.includes('.')) {
+                        // Append '.' to the displayed number
+                        display.textContent += '.';
+                    } else {
+                        // Don't append anything to prevent 2 or more decimal points in a number
+                    }
+                // If decimal key does not follow a number key (could be operator, equal, AC keys) 
+                } else {
+                    display.textContent = '0.';
+                }
+            } else { 
+                // Don't do anything.
+            }
+        }
 
 
 
