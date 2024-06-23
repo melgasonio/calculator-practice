fd42bd5f787fcc2d07a755c858c46008c70db241
unknown <soniomelga@gmail.com>
2024-06-23 08:32:30 +0800

    This reverts e8b0f74

--- app/js/script.js
+++ app/js/script.js
@@ -123,16 +123,17 @@ keys.addEventListener('click', e => {
 
             // If key is DEL
             // Remove only the right most digit or decimal
-            // 
             if (keyContent === 'DEL') {
                 let afterDel = displayContent.slice(0,-1);
-                if (afterDel === '') {
-                    afterDel = '0';
+                // Do not change anything on the display if the  display is a result that was triggered by an operator or equal sign as previous key
+                if (prevKey !== '=' || !operatorKeys.includes(prevKey)) {
+                    if (afterDel === '') {
+                        afterDel = '0';
+                    }
+    
+                    display.textContent = afterDel;
+                    console.log(afterDel);  
                 }
-
-                display.textContent = afterDel;
-                console.log(afterDel);
-
             }
 
             // Increase key presses count by 1
